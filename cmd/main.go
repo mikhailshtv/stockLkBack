5d85ed3a0052b83c177f60b51a07b13b30a6ca9e
Mikhail Shitov <mshitov@iconsoft.ru>
2025-04-28 10:14:28 +0500

    Add goroutines and channels, add log function

--- cmd/main.go
+++ cmd/main.go
@@ -1,29 +1,31 @@
 package main
 
 import (
-	"encoding/json"
-	"fmt"
 	"golang/stockLkBack/internal/repository"
 	"golang/stockLkBack/internal/service"
+	"sync"
 	"time"
 )
 
 func main() {
 	go inerval()
+	go repository.LogAddedEntities()
 	time.Sleep(time.Second * 20)
 }
 
 func inerval() {
 	for range time.Tick(time.Second * 1) {
-		entity := service.NewEntity()
-		repository.CheckAndSaveEntity(entity)
-		entityJSON, _ := json.Marshal(entity)
-		ordersJSON, _ := json.Marshal(repository.OrdersStruct.Entities)
-		productsJSON, _ := json.Marshal(repository.ProductsStruct.Entities)
-		usersJSON, _ := json.Marshal(repository.UsersStruct.Entities)
-		fmt.Printf("Entity: %v\n", string(entityJSON))
-		fmt.Printf("Orders: %v\n", string(ordersJSON))
-		fmt.Printf("Products: %v\n", string(productsJSON))
-		fmt.Printf("Users: %v\n", string(usersJSON))
+		channel := make(chan any)
+		wg := sync.WaitGroup{}
+		wg.Add(2)
+		go func() {
+			channel <- service.NewEntity()
+			wg.Done()
+		}()
+		go func() {
+			repository.CheckAndSaveEntity(<-channel)
+			wg.Done()
+		}()
+		wg.Wait()
 	}
 }
--- internal/repository/repository.go
+++ internal/repository/repository.go
@@ -1,15 +1,22 @@
 package repository
 
 import (
+	"encoding/json"
 	"golang/stockLkBack/internal/model"
+	"log"
+	"sync"
+	"time"
 )
 
 type Entity[T model.Order | model.Product | model.User] struct {
+	Mu       sync.RWMutex
 	Entities []*T
 }
 
 func (entity *Entity[T]) AppendEntity(v T) {
+	entity.Mu.Lock()
 	entity.Entities = append(entity.Entities, &v)
+	entity.Mu.Unlock()
 }
 
 var OrdersStruct = Entity[model.Order]{}
@@ -24,5 +31,40 @@ func CheckAndSaveEntity(entity any) {
 		ProductsStruct.AppendEntity(v)
 	case model.User:
 		UsersStruct.AppendEntity(v)
+	}
+}
+
+func LogAddedEntities() {
+	var ordersLength int
+	var productsLength int
+	var userLength int
+	for range time.Tick(time.Millisecond * 200) {
+		if len(OrdersStruct.Entities) != ordersLength {
+			OrdersStruct.Mu.RLock()
+			ordersJSON, err := json.Marshal(OrdersStruct.Entities[ordersLength:len(OrdersStruct.Entities)])
+			OrdersStruct.Mu.RUnlock()
+			if err == nil {
+				log.Printf("Orders: %v\n", string(ordersJSON))
+			}
+		}
+		if len(ProductsStruct.Entities) != productsLength {
+			ProductsStruct.Mu.RLock()
+			productsJSON, err := json.Marshal(ProductsStruct.Entities[productsLength:len(ProductsStruct.Entities)])
+			ProductsStruct.Mu.RUnlock()
+			if err == nil {
+				log.Printf("Products: %v\n", string(productsJSON))
+			}
+		}
+		if len(UsersStruct.Entities) != userLength {
+			UsersStruct.Mu.RLock()
+			usersJSON, err := json.Marshal(UsersStruct.Entities[userLength:len(UsersStruct.Entities)])
+			UsersStruct.Mu.RUnlock()
+			if err == nil {
+				log.Printf("Users: %v\n", string(usersJSON))
+			}
+		}
+		ordersLength = len(OrdersStruct.Entities)
+		productsLength = len(ProductsStruct.Entities)
+		userLength = len(UsersStruct.Entities)
 	}
 }
