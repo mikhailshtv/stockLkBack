870cb8eaf12ae9d8890cb2ac6dd078ca5c93038c
Mikhail Shitov <mshitov@iconsoft.ru>
2025-05-04 18:39:49 +0500

    add gracefull shutdown

--- cmd/main.go
+++ cmd/main.go
@@ -1,12 +1,19 @@
 package main
 
 import (
+	"context"
 	"golang/stockLkBack/internal/service"
-	"time"
+	"os"
+	"os/signal"
+	"sync"
+	"syscall"
 )
 
 func main() {
-	service.Interval()
-	go service.LogAddedEntities()
-	time.Sleep(time.Second * 20)
+	ctx, stop := signal.NotifyContext(context.Background(), os.Interrupt, syscall.SIGTERM)
+	defer stop()
+	var wg sync.WaitGroup
+	service.Interval(ctx, &wg)
+	service.LogAddedEntities(ctx, &wg)
+	wg.Wait()
 }
